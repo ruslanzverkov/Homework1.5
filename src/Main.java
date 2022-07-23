2bd8d06e2f8a034c82c2b06ca87d738015acd9d3
ruslanzverkov <createmed@mail.ru>
2022-07-23 20:20:23 +0300

    DZ3

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,47 @@ public class Main {
 
         double[] weight = {1.57,7.654,9.986};
 
-        int[] cut=new int[7];
-        cut[0]=5;
-        cut[1]=23;
-        cut[2]=11; //индекс [3]-[6] равно 0
+        float[] cut=new float[7];
+        cut[0]=5f;
+        cut[1]=23f;
+        cut[2]=11f; //индекс [3]-[6] равно 0
 
 //        Задача 2
+
+        for (int i = 0; i < bow.length-1; i++) {
+            System.out.print(bow[i]+",");
+        }
+        System.out.println(bow[bow.length-1]);
+        System.out.println();
+
+        for (int i = 0; i < bow.length; i++) {
+            if (i<bow.length-1){
+                System.out.print(bow[i]+",");
+            }else {
+                System.out.print(bow[i]);
+            }
+        }
+        System.out.println();
+
+
+
+        for (int i = 0; i < weight.length-1; i++) {
+            System.out.print(weight[i]+",");
+        }
+        System.out.println(weight[weight.length-1]);
+        System.out.println();
+
+
+        for (int i = 0; i < cut.length-1; i++) {
+            System.out.print(cut[i]+",");
+        }
+        System.out.println(cut[cut.length-1]);
+        System.out.println();
+
+
+
+//           Третья задача
+
+
     }
 }
