7b198aea947208ae919888a954f8edbc9581e24b
ruslanzverkov <createmed@mail.ru>
2022-07-23 18:35:21 +0300

    DZ 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Первое задание
+
+        int[] bow = new int[8];// или new int[8]{-1,2,3};
+        bow[0]=-1;
+        bow[1]=2;
+        bow[2]=3;
+
+        double[] weight = {1.57,7.654,9.986};
+
+        int[] cut=new int[7];
+        cut[0]=5;
+        cut[1]=23;
+        cut[2]=11; //индекс [3]-[6] равно 0
+
+//        Задача 2
     }
 }
